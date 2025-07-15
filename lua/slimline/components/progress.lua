a40568ae413fe296445968271169b655b728e903
Emilien Jegou <emilien@example.com>
2025-07-15 14:57:24 +0200

    feat: custom progress indicator

--- lua/slimline/components/progress.lua
+++ lua/slimline/components/progress.lua
@@ -5,28 +5,12 @@ local config = slimline.config.configs.progress
 ---@param opts render.options
 ---@return string
 function C.render(opts)
-  local cur = vim.fn.line('.')
   local total = vim.fn.line('$')
-  local primary
-  if cur == 1 then
-    primary = 'Top'
-  elseif cur == total then
-    primary = 'Bot'
-  else
-    primary = string.format('%2d%%%%', math.floor(cur / total * 100))
-  end
-
-  local secondary = ''
-
-  if config.column then
-    local col = vim.fn.col('.')
-    secondary = string.format('%3d', col)
-  end
-
-  primary = string.format('%s %s / %s', config.icon, primary, total)
+  local position = string.format("%d:%d", vim.fn.line('.'), vim.fn.col('.'))
+  local primary = string.format('%s %s / %s', config.icon, position, total)
 
   return slimline.highlights.hl_component(
-    { primary = primary, secondary = secondary },
+    { primary = primary, secondary = '' },
     opts.hls,
     opts.sep,
     opts.direction,
